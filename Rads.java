768ae6eb9980215a6fe5a4f020002e4f0c65caf9
JayCodez7 <Jayden.lau26@ycdsbk12.ca>
2024-02-14 14:14:20 -0500

    Solved Rads

--- Rads.java
+++ Rads.java
@@ -1,13 +1,22 @@
 class Rads extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A computer program that converts degree measures to radian measures
+  * @author:J. Lau
   */
   
   public void run() {
     
-    // start coding here
+    // Declare variables
+    double dblRadians;
+    double dblDegrees;
+
+    //Calculate conversion
+    dblDegrees = readDouble("Input a measurement for degrees: ");
+    dblRadians = Math.toRadians(dblDegrees);
+
+    //Output Radians please
+    System.out.print("A degree value of " + dblDegrees + " is equal to a radians value of " + dblRadians);
     
   }
 }
