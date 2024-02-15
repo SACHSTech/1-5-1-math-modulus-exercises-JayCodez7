6c7efd191eedb3147a2b53841142fb91b42e5edf
JayCodez7 <Jayden.lau26@ycdsbk12.ca>
2024-02-15 11:30:39 -0500

    Cosine solved

--- Cos3rd.java
+++ Cos3rd.java
@@ -1,13 +1,28 @@
 class Cos3rd extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A computer that allows you to calculate the third side if having a contained angle using the cosine law
+  * @author:J. Lau
   */
   
   public void run() {
     
-    // start coding here
-    
+    // declare variables
+    double dblSide1;
+    double dblSide2;
+    double dblAngle;
+    double dblSide3;
+
+    // get sides
+    dblSide1 = readDouble("What is the length of the first side of the triangle? ");
+    dblSide2 = readDouble("What is the length of the second side of the triangle? ");
+    dblAngle = readInt("What is the length of the contained angle of the triangle? ");
+
+    // calculate
+    dblAngle = Math.toRadians(dblAngle);
+    dblSide3 = Math.sqrt((dblSide1 * dblSide1) + (dblSide2 * dblSide2) - 2 * dblSide1 * dblSide2 * Math.cos(dblAngle));
+
+    // print length of missing side
+    System.out.println("The angle of your missing side is " + dblSide3);
   }
 }
