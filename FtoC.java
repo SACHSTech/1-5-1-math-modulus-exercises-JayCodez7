755b170d7fdcb52ac64d4de8e53c5fc03b7cf218
JayCodez7 <Jayden.lau26@ycdsbk12.ca>
2024-02-17 11:45:00 -0500

    Fahrenheit to Celsius solved

--- FtoC.java
+++ FtoC.java
@@ -1,13 +1,24 @@
 class FtoC extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Program that converts a degree of Fahrenheit to celcius
+  * @author:J. Lau
   */
   
   public void run() {
     
-    // start coding here
+    // Declare variables
+    int intFahrenheit;
+    int intCelsius;
+
+    // get Farenheit
+    intFahrenheit = readInt("How many degrees in fahrenheit? ");
+
+    // calculate conversion
+    intCelsius = (((intFahrenheit - 32) * 5)/ 9);
+
+    // output celsius
+    System.out.println(intFahrenheit + " degrees fahrenheit is " + intCelsius + " degrees in Celsius");
     
   }
 }
