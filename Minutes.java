e959d33394508918a7d1af4bbc2628df93bd2fdb
JayCodez7 <Jayden.lau26@ycdsbk12.ca>
2024-02-17 12:35:05 -0500

    Minutes to Days, hours, and minutes solved

--- Minutes.java
+++ Minutes.java
@@ -1,13 +1,31 @@
 class Minutes extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Program that allows you to convert minutes to number of days, hours and minutes that represents.
+  * @author:J. Lau
   */
   
   public void run() {
     
-    // start coding here
+    // declare variables
+    int intMinutes;
+    int intHours;
+    int intDays;
+    int intHoursLeftover;
+    int intMinutesLeftover;
+
+    // get hours
+    intMinutes = readInt("Input the number of minutes: ");
+
+    // calculate
+    intDays = intMinutes / 1440;
+    
+    intHoursLeftover = intDays / 24;
+
+    intMinutesLeftover = intHoursLeftover / 60;
+
+    // output answer
+    System.out.println(intMinutes + " minutes = " + intDays + " days, " + intHoursLeftover + " hours, and " + intMinutesLeftover + " minutes");
     
   }
 }
