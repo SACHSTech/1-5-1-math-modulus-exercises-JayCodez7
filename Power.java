15b69c6f66c7cf04f5fb70c27615b20c66127035
JayCodez7 <Jayden.lau26@ycdsbk12.ca>
2024-02-17 12:01:58 -0500

    Power solved

--- Power.java
+++ Power.java
@@ -1,13 +1,21 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Program that converts a number into an exponent of another number
+  * @author:J. Lau
   */
   
   public void run() {
-    
-    // start coding here
+    // declare variables
+    double dblBase;
+    double dblExponent;
+
+    // get number
+    dblBase = readInt("What is your base? ");
+    dblExponent = readInt("What is your exponent? ");
+
+    // print out number
+    System.out.println("Your number is: " + Math.pow(dblBase, dblExponent));
     
   }
 }
